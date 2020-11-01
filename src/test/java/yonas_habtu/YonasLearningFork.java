fbafa76cbc5a6b8d576103732eb39413a770b310
git config --global user.name <yonashaile12@gmail.com>
2020-11-01 12:01:34 -0600

    Learning Git Fork

--- src/test/java/yonas_habtu/YonasLearningFork.java
+++ src/test/java/yonas_habtu/YonasLearningFork.java
@@ -1,4 +1,8 @@
 package yonas_habtu;
 
 public class YonasLearningFork {
+
+    public static void main(String[] args) {
+        System.out.println("contribution is the key to successful");
+    }
 }
